a2090e7964ecd2c253a08c81bd9eba55deda2298
Dagan3D <GameWorldDan@gmail.com>
2023-09-12 00:32:54 +0300

    add_datatime function added

--- Filtering_data/code/Data_prerocesor.py
+++ Filtering_data/code/Data_prerocesor.py
@@ -1,33 +1,35 @@
 import pandas as pd
 import urllib.request
+import calendar
 import json
-
+import ssl
 
 def load_data (url:str = "https://dt.miet.ru/spinteh/api/", start_id:int = 1, stop_id:int = 13) -> pd.DataFrame:
-  """
-   Запрашивает у сервера данные и сохраняет их в pandas DataFrame
+    """
+    Запрашивает у сервера данные и сохраняет их в pandas DataFrame
 
-  Parameters
-  ----------
-  url : str
-    Адрес где лежит база исходных данных
-  start_id: int
-    Индекс первого рассматриваемого города
-  stop_id: int
-    Индекс последнего рассматриваемого города
+    Parameters
+    ----------
+    url : str
+        Адрес где лежит база исходных данных
+    start_id: int
+        Индекс первого рассматриваемого города
+    stop_id: int
+        Индекс последнего рассматриваемого города
 
-  Returns
-  -------
-  df: pd.DataFrame
-    Массив полученных данных.
-  """
-
-  df = pd.DataFrame()
-  for i in range(start_id, stop_id + 1):
-    with urllib.request.urlopen("https://dt.miet.ru/spinteh/api/" + str(i)) as url:
-        data = json.load(url)
-    df[data['message']['name']] = values = data['message']['data']
-  return df
+    Returns
+    -------
+    df: pd.DataFrame
+        Массив полученных данных.
+    """
+    df = pd.DataFrame()
+    
+    for i in range(start_id, stop_id + 1):
+        with urllib.request.urlopen("https://dt.miet.ru/spinteh/api/" + str(i)) as url:
+            data = json.load(url)
+        df[data['message']['name']]= data['message']['data']
+        
+    return df
 
 
 def weather_data_filter (input_df: pd.DataFrame) -> pd.DataFrame:
@@ -57,7 +59,56 @@ def weather_data_filter (input_df: pd.DataFrame) -> pd.DataFrame:
       filter_df[col][i] = df.smooth[i]
   return filter_df
 
+    
+def add_datatime(df: pd.DataFrame, start_year:int = 2003, end_year:int = 2023) -> pd.DataFrame:
+    """  
+    Добавляет даты к значениям
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Массив данных к которому нужно добавить даты
+    
+    start_year: int
+        Год начала отчёта
+
+    end_year: int
+        Год конца отчета
+
+    Returns
+    -------
+    filter_df: pd.DataFrame
+        Массив данных c датами.
+    """
+    miet_ssl = ssl._create_unverified_context()  # ignore the ssl sertifiacte becayse of the original university SSL certificate
+
+    day_of_month_values = []
+    corresponding_months = []
+    corresponding_years = []
+
+    for year in range(start_year, end_year):
+        for month in range(1, 13):
+            last_day = calendar.monthrange(year, month)[1]
+            days_of_month = list(range(1, last_day + 1))
+            day_of_month_values.extend(days_of_month)
+            corresponding_months.extend([calendar.month_name[month]] * last_day)
+            corresponding_years.extend([year] * last_day)
+
+    # Provided data doesn't exist leap years
+    filtered_values = [
+        (day, month, year)
+        for day, month, year in zip(day_of_month_values, corresponding_months, corresponding_years)
+        if not (day == 29 and month == 'February')
+    ]
+
+    day_of_month_values, corresponding_months, corresponding_years = zip(*filtered_values)  # Unpack the filtered data
+
+    df.insert(0, 'Date', day_of_month_values)
+    df.insert(1, 'Month', corresponding_months)
+    df.insert(2, 'Year', corresponding_years)
+
+    return df
 
 if __name__ == "__main__":
-  df = weather_data_filter(load_data())
+  df = add_datatime(weather_data_filter(load_data()))
   print(df)
