da709b5998cfaf236a82d15dcb5e3b13437d2719
sergeyrbkn <sergey.rbkn@gmail.com>
2023-09-20 02:10:10 +0200

    Fix DB issues and fill data

--- DataBase/main.py
+++ DataBase/main.py
@@ -21,7 +21,7 @@ with db.atomic():
 current_directory = os.path.dirname(__file__)
 csv_file_path = os.path.join(current_directory, '../Filtering_data/source_df.csv')
 source_df = pd.read_csv(csv_file_path)
-city_list = list(source_df.columns[3:])
+city_list = list(source_df.columns[1:])
 data_city = list()  # data to writing into the DB
 count = 1
 
@@ -43,3 +43,72 @@ data_config = [
 
 with db.atomic():
     Configuration.insert_many(data_config, fields=[Configuration.id, Configuration.name]).execute()
+
+################### Pre-filtered data for 'sources' table
+################### id, date, city_id, weather, type_id, config_id
+data_source = list() # main pre-filtered data container
+
+type_id = data_type[0][0]  # 1: pre-filtered
+config_id = data_config[0][0]  # 1: original
+
+count = 1
+
+for i in range(len(data_city)):
+    for j in range(len(source_df)):
+        city_name = data_city[i][1]
+        weather = source_df[city_name][j]  # one weather value
+        date = source_df['Date'][j]
+        city_id = data_city[i][0]  # take an id of the city
+        source = (count, date, city_id, weather, type_id, config_id)
+        data_source.append(source)
+        count += 1
+
+batch_size = 1000
+
+with db.atomic():
+    for i in range(0, len(data_source), batch_size):
+        batch = data_source[i:i + batch_size]
+        Source.insert_many(batch, fields=[
+            Source.id, 
+            Source.date, 
+            Source.city_id, 
+            Source.weather, 
+            Source.type_id, 
+            Source.config_id
+        ]).execute()
+
+#################### Filtered data for 'sources' table
+#################### id, date, city_id, weather, type_id, config_id
+csv_file_path = os.path.join(current_directory, '../Filtering_data/filtered_df.csv')
+filtered_df = pd.read_csv(csv_file_path)
+
+data_filtered = list()  # main filtered data container
+
+type_id = data_type[1][0]  # 2: 'filtered' type of data (after filtering)
+config_id = data_config[0][0]  # 1: original
+
+f_count = len(source_df) * len(data_city) + 1
+
+for i in range(len(data_city)):
+    for j in range(len(filtered_df)):
+        city_name = data_city[i][1]
+        weather = filtered_df[city_name][j]  # one weather value
+        date = filtered_df['Date'][j]
+        city_id = data_city[i][0]  # take an id of the city
+        f_source = (f_count, date, city_id, weather, type_id, config_id)
+        data_filtered.append(f_source)
+        f_count += 1
+
+batch_size = 1000
+
+with db.atomic():
+    for i in range(0, len(data_filtered), batch_size):
+        batch = data_filtered[i:i + batch_size]
+        Source.insert_many(batch, fields=[
+            Source.id, 
+            Source.date, 
+            Source.city_id, 
+            Source.weather, 
+            Source.type_id, 
+            Source.config_id
+        ]).execute()
